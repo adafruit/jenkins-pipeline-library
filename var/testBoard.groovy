c76dad25397b4d785d6b0ac16483db9b4e503c4b
Todd Treece <toddtreece@gmail.com>
2016-08-24 16:29:52 -0400

    add testBoard global function

---
+++ var/testBoard.groovy
@@ -0,0 +1,34 @@
+def call(String repo, String label) {
+
+  node(label) {
+
+    currentBuild.result = "SUCCESS"
+
+    try {
+
+       stage 'Clone'
+
+         git url: repo
+
+       stage 'Test'
+
+         lock(env.NODE_NAME + "-" + label) {
+           sh 'prove --formatter TAP::Formatter::Jenkins /home/pi/test.t'
+         }
+
+       stage 'Publish'
+
+         step([$class: "TapPublisher", testResults: "**/target/tap-unit.log"])
+
+
+    } catch (err) {
+
+        currentBuild.result = "FAILURE"
+
+        throw err
+
+    }
+
+  }
+
+}

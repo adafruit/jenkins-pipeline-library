3c913d1d2a43201e1cb3cc61b9f46f2da4a9c41b
Todd Treece <toddtreece@gmail.com>
2016-08-26 14:09:48 -0400

    add verifyExamples utility

---
+++ vars/verifyExamples.groovy
@@ -0,0 +1,11 @@
+def call(String platform) {
+
+  new File("examples").eachDirRecurse() { dir ->
+
+    dir.eachFileMatch(~/.*.ino/) { file ->
+      sh "arduino --board \$${platform} --verify ${file.getPath()} 2>&1"
+    }
+
+  }
+
+}

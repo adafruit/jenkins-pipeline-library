258b1282df5fa25ed6cffa8e30f34717f8c6c6e0
Todd Treece <toddtreece@gmail.com>
2016-08-26 15:52:31 -0400

    point to local examples folder

--- vars/verifyExamples.groovy
+++ vars/verifyExamples.groovy
@@ -1,6 +1,6 @@
 def call(String platform) {
 
-  new File("examples").eachDirRecurse() { dir ->
+  new File("./examples").eachDirRecurse() { dir ->
 
     dir.eachFileMatch(~/.*.ino/) { file ->
       sh "arduino --board \$${platform} --verify ${file.getPath()} 2>&1"

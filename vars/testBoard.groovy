bd1cd1bdf50a493998eccb0a6d491458e981f8fc
Todd Treece <toddtreece@gmail.com>
2016-08-29 09:22:57 -0400

    add testBoard function

---
+++ vars/testBoard.groovy
@@ -0,0 +1,15 @@
+def call(String platform) {
+
+  generateTest()
+
+  sh """#!/bin/bash
+  arduino --board \$${platform} --save-prefs
+  declare -a tests
+  tests=(\$(find .tests -name '*.ino'))
+  for test in \"\${tests[@]}\"; do
+    arduino --board \$${platform} --port \$${platform}_PORT --upload $test 2>&1
+    prove --formatter TAP::Formatter::Jenkins ./tests/arduino_serial.t :: --port \$${platform}_PORT
+  done
+  """
+
+}

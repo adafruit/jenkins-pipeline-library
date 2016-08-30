d0710237ae155be9e32c3386fe62bbd7006093fc
Todd Treece <toddtreece@gmail.com>
2016-08-30 11:47:41 -0400

    wrap arduino dir in quotes

--- vars/installArduinoIDE.groovy
+++ vars/installArduinoIDE.groovy
@@ -3,7 +3,7 @@ def call(target, version) {
   sh """#!/bin/bash
   mkdir -p \$HOME/ide
   cd \$HOME/ide
-  if[ ! -d arduino-${version} ]; then
+  if[ ! -d \"arduino-${version}\" ]; then
     wget https://downloads.arduino.cc/arduino-${version}-linuxarm.tar.xz
     tar xf arduino-${version}-linuxarm.tar.xz
     mkdir arduino-${version}/portable

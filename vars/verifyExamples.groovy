78a96c51c6d2f5a91219fb94f38476e0c57589b7
Todd Treece <toddtreece@gmail.com>
2016-08-30 09:18:57 -0400

    process example names on worker node

--- vars/verifyExamples.groovy
+++ vars/verifyExamples.groovy
@@ -7,23 +7,31 @@ def call(boards) {
     // bind the label variable before the closure
     def platform = board
     def examples = []
+    def example_names = []
 
     node(platform) {
+
       checkout scm
       sh "arduino --board \$${platform} --save-prefs"
+
       examples = sh(returnStdout: true, script: "find examples -name '*.ino' | sort").trim().split("\n")
+
+      for (int i = 0; i < examples.size(); i++) {
+        example_names[i] = sh(returnStdout: true, script: "basename ${examples[i]} .ino")
+      }
+
     }
 
     def builders = [:]
 
     for (int i = 0; i < examples.size(); i++) {
 
-      def example = sh(returnStdout: true, script: "basename ${examples[i]} .ino")
+      def example = examples[i]
 
-      builders[example] {
+      builders[example_names[i]] {
 
         node(platform) {
-          echo "Verifying ${example} on ${platform}"
+          echo "Verifying ${example_names[i]}.ino on ${platform}"
           sh "arduino --board \$${platform} --verify ${example} 2>&1"
         }
 

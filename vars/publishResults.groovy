eaacfb8e41e62a57a10291b6e6b105fb522677ac
Todd Treece <toddtreece@gmail.com>
2016-09-02 11:44:07 -0400

    move workspace cleanup to remote node

--- vars/publishResults.groovy
+++ vars/publishResults.groovy
@@ -6,13 +6,13 @@ def call(boards) {
 
     node(board) {
       step([$class: 'TapPublisher', discardOldReports: false, enableSubtests: false, failIfNoResults: false, failedTestsMarkBuildAsFailure: true, flattenTapResult: false, includeCommentDiagnostics: false, outputTapToConsole: true, planRequired: true, showOnlyFailures: false, skipIfBuildNotOk: false, stripSingleParents: false, testResults: '**/*.tap', todoIsFailure: false, validateNumberOfTests: true, verbose: true])
+      step([$class: 'WsCleanup', deleteDirs: true, patterns: [[pattern: '**', type: 'INCLUDE']]])
     }
 
   }
 
   node('master') {
     step([$class: 'GitHubCommitStatusSetter'])
-    step([$class: 'WsCleanup', deleteDirs: true, patterns: [[pattern: '**', type: 'INCLUDE']]])
   }
 
 }
